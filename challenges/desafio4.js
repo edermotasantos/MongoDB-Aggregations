76c8fa804e7c73b045fce9db2e562dac7adf1a11
Eder Santos <3derms@gmail.com>
2021-10-16 22:05:49 -0300

    Cria uma pipeline que retorna documentos com o novo campo title_split

---
+++ challenges/desafio4.js
@@ -0,0 +1,27 @@
+/** Source: https://docs.mongodb.com/manual/reference/operator/aggregation/split/ */
+/** Source: https://docs.mongodb.com/manual/reference/operator/query/size/ */
+db.movies.aggregate(
+  [
+    {
+      $addFields: {
+        title_split: {
+          $split: [ "$title", " " ],
+        },
+      },
+    },
+    {
+      $match: { title_split: { $size: 1 } },
+    },
+    {
+      $sort: {
+        title: 1,
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        title_split: 1,
+      },
+    },
+  ],
+);
